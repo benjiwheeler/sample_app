d0c1cbf5c637010c71e6955b768b6cd193357582
Ben Wheeler <wheeler.benjamin@gmail.com>
2013-10-08 16:53:15 -0400

    deprecated some lines for rails 4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,8 @@ SampleApp::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+# deprecated in rails 4
+#  config.active_record.mass_assignment_sanitizer = :strict
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
@@ -34,7 +35,8 @@ SampleApp::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  # false = faster
+  config.assets.debug = false
 
   config.log_level = :debug
   config.eager_load = false
