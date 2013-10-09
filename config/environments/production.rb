d6a885f78853f186eb7e8341f976ece9cbf22a79
Ben Wheeler <wheeler.benjamin@gmail.com>
2013-10-09 15:21:35 -0400

    try again to set css files to be precompiled in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ SampleApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['*.js', '*.css', '*.css.erb']
+#  config.assets.precompile += ['*.js', '*.css', '*.css.erb']
+   config.assets.precompile += %w( application.js application.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
